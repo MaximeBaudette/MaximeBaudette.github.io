6b2d1dd32c027a186c702fb7b0e58855c16e629b
Maxime Baudette <maxime.baudette@gmail.com>
2026-04-27 11:49:43 +0200

    Fix cache-bust plugin path and allow PurgeCSS to strip unused FA icons

--- purgecss.config.js
+++ purgecss.config.js
@@ -4,21 +4,15 @@ module.exports = {
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
   safelist: [
-    // Dynamic theme classes
     /^(light|dark)-/,
-    /^fa-/,
-    /^ti-/,
-    // Bootstrap JS-driven classes
     "show",
     "active",
     "collapsing",
     "collapse",
     "in",
     "open",
-    // MDBootstrap
     "waves-effect",
     "ripple",
-    // Animation classes
     "animated",
     "fade",
   ],
